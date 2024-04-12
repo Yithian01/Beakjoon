8d17d85b62739bbb4d863e241aec934897a188bd
Yithian01 <dbwlsdud10@naver.com>
2024-04-12 18:59:32 +0900

    complete bj17828

--- 2024_1st_semester/bj17828.py
+++ 2024_1st_semester/bj17828.py
@@ -6,14 +6,32 @@ ma = {}
 for i in range(26):
     tmp = 65 + i
     tmp = chr(tmp)
-    ma[tmp] = i+1
+    ma[i+1] = tmp
 
 
 
 n, m = map(int, input().split())
-if 26 * n < m or m - n < 0 : continue
+if 26 * n < m or m - n < 0 : 
+    print('!')
+    exit(0)
 
 
+dp = [1] * n
+ans = m - n
+
+for i in range(n):
+    if ans <= 0:
+        break
+    ans += 1
+    tmp = 1
+    while tmp < 26:
+        tmp += 1
+        if ans - tmp <= 0:
+            break
+    
+    ans -= tmp
+    dp[i] = tmp
 
 
-
+for i in range(n-1, -1, -1):  
+    print(ma[dp[i]],end='')
