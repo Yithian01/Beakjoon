e9c76cf4b7e22c6caf4e1efb4acb686670ec66d8
Yithian01 <dbwlsdud10@naver.com>
2024-04-12 18:44:46 +0900

    I studing hard

--- 2024_1st_semester/bj17828.py
+++ 2024_1st_semester/bj17828.py
@@ -1,31 +1,19 @@
 # 26 * n => 최대 
 import sys
 input = sys.stdin.readline
+ma = {}
 
-n, m = map(int,input().split())
-
-if m > (26 * n) and m < (1 * n): 
-    print('!')
-    exit(0)
-
-ans = m - n
-q = [1] * n
-for i in range(n):
-    tmp = q[i]
-    for j in range(25):
-        if ans - tmp + 1 < 0: break
-        tmp += 1
-        
-    if tmp != 1:
-        ans -= tmp
-    if ans == 0:
-        break
-
-    q[i] = tmp
+for i in range(26):
+    tmp = 65 + i
+    tmp = chr(tmp)
+    ma[tmp] = i+1
 
 
-res = 0
-for i in range(len(q)):
-    res += q[i]
 
-print(f'res = {res}')
+n, m = map(int, input().split())
+if 26 * n < m or m - n < 0 : continue
+
+
+
+
+
